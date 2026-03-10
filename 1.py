9335b8bde4921de40d7857c1a36a63e324b11231
iMelinay <selinayszg@gmail.com>
2026-03-10 22:07:58 +0300

    Add files via upload
    Değişken Tanımlama, değişken adı nasıl yazılır, sayısal sözel değerlerin çıktıdaki sonucu için video içerisinde gördüğüm ve denemiş olduğum örnekler.

---
+++ 1.py
@@ -0,0 +1,58 @@
+# print (5000 - (5000 * 0.27))
+
+# maasirem = 5000
+# maasali = 4000
+# vergi = 0.27
+
+# print ( maasirem - (maasirem * vergi))
+
+# print (maasali - (maasali * vergi))
+
+
+# number1 = 10
+
+# _number1= 10
+
+
+# number1 = 10
+# print (number1)
+# number1 = 20
+# print (number1)
+
+# number1+= 30
+# print (number1)
+
+
+# age = 20
+
+# AGE = 30
+
+# print(age)
+# print(AGE)
+
+
+# X = 1                   int
+# Y = 2.3                 float
+# name = 'Alya'           string
+# isStudent = TRUE        bool
+
+
+
+# a = 10
+# b = 20
+# print (a + b)
+
+
+# a = '10'
+# b = '20'
+# print ( a + b)
+
+
+# firstName= "Ela "
+# LastName = "Kaya"
+
+# print (firstName + LastName)
+
+
+
+X, Y,name, isStudent = (1, 2.3, "Ela", True)
